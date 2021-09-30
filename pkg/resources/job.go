c1a56adcbd3579b46548ee014016b4d05297f68f
GtheSheep <gary.james19@gmail.com>
2021-09-30 20:34:12 +0100

    Update

--- pkg/resources/job.go
+++ pkg/resources/job.go
@@ -35,6 +35,7 @@ func ResourceJob() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceJobCreate,
 		Read:   resourceJobRead,
+		Update: resourceJobUpdate,
 
 		Schema: jobSchema,
 	}
@@ -111,6 +112,62 @@ func resourceJobCreate(d *schema.ResourceData, m interface{}) error {
 
 		if err != nil {
 			log.Printf("Error creating job")
+			return err
+		}
+		defer resp.Body.Close()
+
+		job := new(dbt_cloud.Job)
+		err = json.NewDecoder(resp.Body).Decode(&job)
+		if err != nil {
+			return err
+		}
+
+		return err
+	}
+
+	return nil
+}
+
+func resourceJobUpdate(d *schema.ResourceData, m interface{}) error {
+	token := d.Get("token").(string)
+
+	account_id := d.Get("account_id").(int)
+	project_id := d.Get("project_id").(int)
+	job_id := d.Get("job_id").(int)
+	environment_id := d.Get("environment_id").(int)
+	name := d.Get("name").(string)
+	execute_steps := d.Get("execute_steps").([]string)
+	dbt_version := d.Get("dbt_version").(string)
+	triggers := d.Get("triggers").(dbt_cloud.JobTrigger)
+	settings := d.Get("settings").(dbt_cloud.JobSettings)
+	state := d.Get("state").(int)
+	generate_docs := d.Get("generate_docs").(bool)
+	schedule := d.Get("schedule").(dbt_cloud.JobSchedule)
+
+	if token != "" {
+		newJob := dbt_cloud.JobData{
+			Account_Id:     account_id,
+			Project_Id:     project_id,
+			Environment_Id: environment_id,
+			Name:           name,
+			Execute_Steps:  execute_steps,
+			Dbt_Version:    dbt_version,
+			Triggers:       triggers,
+			Settings:       settings,
+			State:          state,
+			Generate_Docs:  generate_docs,
+			Schedule:       schedule,
+		}
+		url := fmt.Sprintf("https://cloud.getdbt.com/api/v2/accounts/%s/jobs/%s/", account_id, job_id)
+		newJobData, err := json.Marshal(newJob)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(newJobData))
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Authorization", fmt.Sprintf("Token %s", token))
+		client := &http.Client{Timeout: 60 * time.Second}
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Printf("Error updating job")
 			return err
 		}
 		defer resp.Body.Close()
