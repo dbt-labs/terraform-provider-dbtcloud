e69dc0c9ae0f6c5deb2ac2321cb40255f929c261
Gary James <gary.james19@gmail.com>
2021-10-24 13:53:40 +0100

    Update job.go

--- pkg/dbt_cloud/job.go
+++ pkg/dbt_cloud/job.go
@@ -56,7 +56,7 @@ type Job struct {
 }
 
 func (c *Client) GetJob(jobID string) (*Job, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/accounts/%s/v2/jobs/%s/", c.HostURL, strconv.Itoa(c.AccountID), jobID), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/v2/accounts/%s/jobs/%s/", c.HostURL, strconv.Itoa(c.AccountID), jobID), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -132,7 +132,7 @@ func (c *Client) CreateJob(projectId int, environmentId int, name string, execut
 		return nil, err
 	}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/accounts/%s/v2/jobs/", c.HostURL, strconv.Itoa(c.AccountID)), strings.NewReader(string(newJobData)))
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/v2/accounts/%s/jobs/", c.HostURL, strconv.Itoa(c.AccountID)), strings.NewReader(string(newJobData)))
 	if err != nil {
 		return nil, err
 	}
@@ -157,7 +157,7 @@ func (c *Client) UpdateJob(jobId string, job Job) (*Job, error) {
 		return nil, err
 	}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/accounts/%s/v2/%s/jobs/%s/", c.HostURL, strconv.Itoa(c.AccountID), jobId), strings.NewReader(string(jobData)))
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/v2/accounts/%s/jobs/%s/", c.HostURL, strconv.Itoa(c.AccountID), jobId), strings.NewReader(string(jobData)))
 	if err != nil {
 		return nil, err
 	}
