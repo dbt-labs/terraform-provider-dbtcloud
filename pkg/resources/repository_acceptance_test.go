3abc304b8c07c07da9b957989b30e3ba726f36c0
GtheSheep <gary.james19@gmail.com>
2022-03-22 23:25:03 +0100

    fix tests

--- pkg/resources/repository_acceptance_test.go
+++ pkg/resources/repository_acceptance_test.go
@@ -15,7 +15,7 @@ import (
 func TestAccDbtCloudRepositoryResource(t *testing.T) {
 
 	repoUrl := "https://github.com/GtheSheep/terraform-provider-dbt-cloud.git"
-	repoUrl2 := "https://github.com/GtheSheep/dbt-core.git"
+	// 	repoUrl2 := "https://github.com/GtheSheep/dbt-core.git"
 	projectName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 
 	resource.Test(t, resource.TestCase{
