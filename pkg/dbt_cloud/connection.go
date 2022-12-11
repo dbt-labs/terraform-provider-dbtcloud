f1ab75fd6603406e00de6ef7c7c53d0dfd02d93f
GtheSheep <gary.james19@gmail.com>
2022-12-11 21:16:46 +0000

    feature - Add Databricks connection

--- pkg/dbt_cloud/connection.go
+++ pkg/dbt_cloud/connection.go
@@ -9,18 +9,19 @@ import (
 )
 
 type ConnectionDetails struct {
-	Account                string `json:"account,omitempty"`
-	Database               string `json:"database,omitempty"`
-	DBName                 string `json:"dbname,omitempty"`
-	Warehouse              string `json:"warehouse,omitempty"`
-	AllowSSO               *bool  `json:"allow_sso,omitempty"`
-	ClientSessionKeepAlive *bool  `json:"client_session_keep_alive,omitempty"`
-	Role                   string `json:"role,omitempty"`
-	OAuthClientID          string `json:"oauth_client_id,omitempty"`
-	OAuthClientSecret      string `json:"oauth_client_secret,omitempty"`
-	Host                   string `json:"hostname,omitempty"`
-	Port                   int    `json:"port,omitempty"`
-	TunnelEnabled          *bool  `json:"tunnel_enabled,omitempty"`
+	Account                string                      `json:"account,omitempty"`
+	Database               string                      `json:"database,omitempty"`
+	DBName                 string                      `json:"dbname,omitempty"`
+	Warehouse              string                      `json:"warehouse,omitempty"`
+	AllowSSO               *bool                       `json:"allow_sso,omitempty"`
+	ClientSessionKeepAlive *bool                       `json:"client_session_keep_alive,omitempty"`
+	Role                   string                      `json:"role,omitempty"`
+	OAuthClientID          string                      `json:"oauth_client_id,omitempty"`
+	OAuthClientSecret      string                      `json:"oauth_client_secret,omitempty"`
+	Host                   string                      `json:"hostname,omitempty"`
+	Port                   int                         `json:"port,omitempty"`
+	TunnelEnabled          *bool                       `json:"tunnel_enabled,omitempty"`
+	AdapterDetails         DatabricksCredentialDetails `json:"connection_details,omitempty"`
 }
 
 type Connection struct {
@@ -67,33 +68,107 @@ func (c *Client) GetConnection(connectionID, projectID string) (*Connection, err
 	return &connectionResponse.Data, nil
 }
 
-func (c *Client) CreateConnection(projectID int, name string, connectionType string, isActive bool, account string, database string, warehouse string, role string, allowSSO *bool, clientSessionKeepAlive *bool, oAuthClientID string, oAuthClientSecret string, hostName string, port int, tunnelEnabled *bool) (*Connection, error) {
+func (c *Client) CreateConnection(projectID int, name string, connectionType string, isActive bool, account string, database string, warehouse string, role string, allowSSO *bool, clientSessionKeepAlive *bool, oAuthClientID string, oAuthClientSecret string, hostName string, port int, tunnelEnabled *bool, httpPath string, catalog string) (*Connection, error) {
 	state := STATE_ACTIVE
 	if !isActive {
 		state = STATE_DELETED
 	}
 
-	connectionDetails := ConnectionDetails{
-		Account:           account,
-		Warehouse:         warehouse,
-		Role:              role,
-		OAuthClientID:     oAuthClientID,
-		OAuthClientSecret: oAuthClientSecret,
-		Host:              hostName,
-		Port:              port,
-	}
-	if connectionType == "snowflake" {
-		connectionDetails.Database = database
-		connectionDetails.AllowSSO = allowSSO
-		connectionDetails.ClientSessionKeepAlive = clientSessionKeepAlive
-	} else if connectionType == "redshift" {
-		connectionDetails.TunnelEnabled = tunnelEnabled
-		connectionDetails.DBName = database
-	} else {
-		connectionDetails.TunnelEnabled = tunnelEnabled
-		connectionDetails.DBName = database
-	}
+	connectionDetails := ConnectionDetails{}
+	if connectionType == "adapter" {
+		noValidation := DatabricksCredentialFieldMetadataValidation{
+			Required: false,
+		}
 
+		typeMetadata := DatabricksCredentialFieldMetadata{
+			Label:        "Connection type",
+			Description:  "",
+			Field_Type:   "hidden",
+			Encrypt:      false,
+			Overrideable: false,
+			Validation:   noValidation,
+		}
+		typeField := DatabricksCredentialField{
+			Metadata: typeMetadata,
+			Value:    "databricks",
+		}
+
+		hostMetadata := DatabricksCredentialFieldMetadata{
+			Label:        "Server Hostname",
+			Description:  "The hostname of the Databricks cluster or SQL warehouse",
+			Field_Type:   "text",
+			Encrypt:      false,
+			Overrideable: false,
+			Validation:   noValidation,
+		}
+		hostField := DatabricksCredentialField{
+			Metadata: hostMetadata,
+			Value:    hostName,
+		}
+
+		httpPathMetadata := DatabricksCredentialFieldMetadata{
+			Label:        "HTTP Path",
+			Description:  "The HTTP path of the Databricks cluster or SQL warehouse",
+			Field_Type:   "text",
+			Encrypt:      false,
+			Overrideable: false,
+			Validation:   noValidation,
+		}
+		httpPathField := DatabricksCredentialField{
+			Metadata: httpPathMetadata,
+			Value:    httpPath,
+		}
+
+		fieldOrder := []string{"type", "host", "http_path"}
+		fields := map[string]DatabricksCredentialField{
+			"type":      typeField,
+			"host":      hostField,
+			"http_path": httpPathField,
+		}
+
+		if catalog != "" {
+			catalogMetadata := DatabricksCredentialFieldMetadata{
+				Label:        "Catalog",
+				Description:  "Optional: Catalog name if Unity Catalog is enabled in your Databricks workspace.  Only available in dbt version 1.1 and later",
+				Field_Type:   "text",
+				Encrypt:      false,
+				Overrideable: true,
+				Validation:   noValidation,
+			}
+			catalogField := DatabricksCredentialField{
+				Metadata: catalogMetadata,
+				Value:    catalog,
+			}
+			fieldOrder = append(fieldOrder, "catalog")
+			fields["catalog"] = catalogField
+		}
+
+		databricksCredentialDetails := DatabricksCredentialDetails{
+			Fields:      fields,
+			Field_Order: fieldOrder,
+		}
+
+		connectionDetails.AdapterDetails = databricksCredentialDetails
+	} else {
+		connectionDetails.Account = account
+		connectionDetails.Warehouse = warehouse
+		connectionDetails.Role = role
+		connectionDetails.OAuthClientID = oAuthClientID
+		connectionDetails.OAuthClientSecret = oAuthClientSecret
+		connectionDetails.Host = hostName
+		connectionDetails.Port = port
+		if connectionType == "snowflake" {
+			connectionDetails.Database = database
+			connectionDetails.AllowSSO = allowSSO
+			connectionDetails.ClientSessionKeepAlive = clientSessionKeepAlive
+		} else if connectionType == "redshift" {
+			connectionDetails.TunnelEnabled = tunnelEnabled
+			connectionDetails.DBName = database
+		} else {
+			connectionDetails.TunnelEnabled = tunnelEnabled
+			connectionDetails.DBName = database
+		}
+	}
 	newConnection := Connection{
 		AccountID: c.AccountID,
 		ProjectID: projectID,
