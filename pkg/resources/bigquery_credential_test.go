e52f5452a6a4c74e138a2f86f29cc3ca540a285e
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-03-09 19:40:37 +0100

    Add BQ credentials resource and datasource

---
+++ pkg/resources/bigquery_credential_test.go
@@ -0,0 +1,115 @@
+package resources_test
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/gthesheep/terraform-provider-dbt-cloud/pkg/dbt_cloud"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccDbtCloudBigQueryCredentialResource(t *testing.T) {
+
+	projectName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	dataset := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDbtCloudBigQueryCredentialDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDbtCloudBigQueryCredentialResourceBasicConfig(projectName, dataset),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDbtCloudBigQueryCredentialExists("dbt_cloud_bigquery_credential.test_credential"),
+					resource.TestCheckResourceAttr("dbt_cloud_bigquery_credential.test_credential", "dataset", dataset),
+				),
+			},
+			// RENAME
+			// MODIFY
+			// IMPORT
+			{
+				ResourceName:            "dbt_cloud_bigquery_credential.test_credential",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"password"},
+			},
+		},
+	})
+}
+
+func testAccDbtCloudBigQueryCredentialResourceBasicConfig(projectName, dataset string) string {
+	return fmt.Sprintf(`
+resource "dbt_cloud_project" "test_project" {
+  name        = "%s"
+}
+resource "dbt_cloud_bigquery_credential" "test_credential" {
+    is_active = true
+    project_id = dbt_cloud_project.test_project.id
+    dataset = "%s"
+    num_threads = 3
+}
+`, projectName, dataset)
+}
+
+func testAccCheckDbtCloudBigQueryCredentialExists(resource string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[resource]
+		if !ok {
+			return fmt.Errorf("Not found: %s", resource)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+		projectId, err := strconv.Atoi(strings.Split(rs.Primary.ID, dbt_cloud.ID_DELIMITER)[0])
+		if err != nil {
+			return fmt.Errorf("Can't get projectId")
+		}
+		credentialId, err := strconv.Atoi(strings.Split(rs.Primary.ID, dbt_cloud.ID_DELIMITER)[1])
+		if err != nil {
+			return fmt.Errorf("Can't get credentialId")
+		}
+
+		apiClient := testAccProvider.Meta().(*dbt_cloud.Client)
+		_, err = apiClient.GetBigQueryCredential(projectId, credentialId)
+		if err != nil {
+			return fmt.Errorf("error fetching item with resource %s. %s", resource, err)
+		}
+		return nil
+	}
+}
+
+func testAccCheckDbtCloudBigQueryCredentialDestroy(s *terraform.State) error {
+	apiClient := testAccProvider.Meta().(*dbt_cloud.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "dbt_cloud_bigquery_credential" {
+			continue
+		}
+		projectId, err := strconv.Atoi(strings.Split(rs.Primary.ID, dbt_cloud.ID_DELIMITER)[0])
+		if err != nil {
+			return fmt.Errorf("Can't get projectId")
+		}
+		credentialId, err := strconv.Atoi(strings.Split(rs.Primary.ID, dbt_cloud.ID_DELIMITER)[1])
+		if err != nil {
+			return fmt.Errorf("Can't get credentialId")
+		}
+
+		_, err = apiClient.GetBigQueryCredential(projectId, credentialId)
+		if err == nil {
+			return fmt.Errorf("BigQuery credential still exists")
+		}
+		notFoundErr := "did not find"
+		expectedErr := regexp.MustCompile(notFoundErr)
+		if !expectedErr.Match([]byte(err.Error())) {
+			return fmt.Errorf("expected %s, got %s", notFoundErr, err)
+		}
+	}
+
+	return nil
+}
