6883944f76f9747ec56082c9c66f94a0f25dfb76
GtheSheep <gary.james19@gmail.com>
2023-03-29 17:40:47 +0100

    feature - Add permissions for groups

--- pkg/resources/group.go
+++ pkg/resources/group.go
@@ -2,12 +2,34 @@ package resources
 
 import (
 	"context"
-	"fmt"
 	"strconv"
 
 	"github.com/gthesheep/terraform-provider-dbt-cloud/pkg/dbt_cloud"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+var (
+	permissionSets = []string{
+		"owner",
+		"member",
+		"account_admin",
+		"admin",
+		"database_admin",
+		"git_admin",
+		"team_admin",
+		"job_admin",
+		"job_viewer",
+		"analyst",
+		"developer",
+		"stakeholder",
+		"readonly",
+		"project_creator",
+		"account_viewer",
+		"metadata_only",
+		"webhooks_only",
+	}
 )
 
 func ResourceGroup() *schema.Resource {
@@ -43,6 +65,30 @@ func ResourceGroup() *schema.Resource {
 					Type: schema.TypeString,
 				},
 			},
+			"group_permissions": &schema.Schema{
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"permission_set": {
+							Type:         schema.TypeString,
+							Required:     true,
+							Description:  "Set of permissions to apply",
+							ValidateFunc: validation.StringInSlice(permissionSets, false),
+						},
+						"project_id": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Project ID to apply this permission to for this group",
+						},
+						"all_projects": {
+							Type:        schema.TypeBool,
+							Required:    true,
+							Description: "Whether or not to apply this permission to all projects for this group",
+						},
+					},
+				},
+			},
 		},
 
 		Importer: &schema.ResourceImporter{
@@ -65,6 +111,25 @@ func resourceGroupCreate(ctx context.Context, d *schema.ResourceData, m interfac
 	}
 
 	group, err := c.CreateGroup(name, assignByDefault, ssoMappingGroups)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	groupPermissionsRaw := d.Get("group_permissions").(*schema.Set).List()
+	groupPermissions := make([]dbt_cloud.GroupPermission, len(groupPermissionsRaw))
+	for i, p := range groupPermissionsRaw {
+		permission := p.(map[string]interface{})
+		groupPermission := dbt_cloud.GroupPermission{
+			GroupID:     *group.ID,
+			AccountID:   group.AccountID,
+			Set:         permission["permission_set"].(string),
+			ProjectID:   permission["project_id"].(int),
+			AllProjects: permission["all_projects"].(bool),
+		}
+		groupPermissions[i] = groupPermission
+	}
+
+	_, err = c.UpdateGroupPermissions(*group.ID, groupPermissions)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -103,12 +168,49 @@ func resourceGroupRead(ctx context.Context, d *schema.ResourceData, m interface{
 	if err := d.Set("sso_mapping_groups", group.SSOMappingGroups); err != nil {
 		return diag.FromErr(err)
 	}
+	permissions := make([]interface{}, len(group.Permissions))
+	for i, permission := range group.Permissions {
+		p := make(map[string]interface{})
+		p["permission_set"] = permission.Set
+		p["project_id"] = permission.ProjectID
+		p["all_projects"] = permission.AllProjects
+		permissions[i] = p
+	}
+	if err := d.Set("group_permissions", permissions); err != nil {
+		return diag.FromErr(err)
+	}
 
 	return diags
 }
 
 func resourceGroupUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	fmt.Printf("Groups do not currently support updates")
+	c := m.(*dbt_cloud.Client)
+
+	groupID, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if d.HasChange("group_permissions") {
+		// TODO(GtheSheep): Extract this to a function
+		groupPermissionsRaw := d.Get("group_permissions").(*schema.Set).List()
+		groupPermissions := make([]dbt_cloud.GroupPermission, len(groupPermissionsRaw))
+		for i, p := range groupPermissionsRaw {
+			permission := p.(map[string]interface{})
+			groupPermission := dbt_cloud.GroupPermission{
+				GroupID:     groupID,
+				AccountID:   c.AccountID,
+				Set:         permission["permission_set"].(string),
+				ProjectID:   permission["project_id"].(int),
+				AllProjects: permission["all_projects"].(bool),
+			}
+			groupPermissions[i] = groupPermission
+		}
+		_, err = c.UpdateGroupPermissions(groupID, groupPermissions)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
 	return resourceGroupRead(ctx, d, m)
 }
 
