d09276d681702591407e5c662e6eec41a8ca3ce3
GtheSheep <gary.james19@gmail.com>
2023-03-29 02:16:55 +0100

    fix - Move github_installation_id instruction

--- examples/resources/dbt_cloud_repository/resource.tf
+++ examples/resources/dbt_cloud_repository/resource.tf
@@ -1,12 +1,3 @@
-# In order to find the github_installation_id, you can log in to dbt Cloud, replace <dbt_cloud_url> by your dbt Cloud URL and run the following commands in the Google Chrome console:
-
-# ```
-# dbt_cloud_api_result = await (fetch('https://<dbt_cloud_url>/api/v2/integrations/github/installations/').then(res => res.json()));
-# console.log("github_application_id: " + dbt_cloud_api_result.filter(res => res["access_tokens_url"].includes("github"))[0]["id"]);
-# ```
-
-# Alternatively, you can go to the page https://<dbt_cloud_url>/api/v2/integrations/github/installations/ and read the value of "id"
-
 resource "dbt_cloud_repository" "github_repo" {
   project_id = 1234
   remote_url = "git://github.com/<github_org>/<github_repo>.git"
