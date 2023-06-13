1e9554e2e8c84bd38fd259c549775523e8eae6fb
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-06-13 13:01:59 +0200

    Feature/pl warehouse connection (#148)

---
+++ pkg/data_sources/privatelink_endpoint_acceptance_test.go
@@ -0,0 +1,64 @@
+package data_sources_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDbtCloudPrivatelinkEndpointDataSource(t *testing.T) {
+
+	// we only test this explicitly as we can't create resources and need to read from existing ones
+	if os.Getenv("DBT_ACCEPTANCE_TEST_PRIVATE_LINK") != "" {
+
+		endpointName := os.Getenv("DBT_ACCEPTANCE_TEST_PRIVATE_LINK_NAME")
+		endpointURL := os.Getenv("DBT_ACCEPTANCE_TEST_PRIVATE_LINK_URL")
+
+		// different configurations whether we provide the endpoint name and/or url
+		config := privatelinkEndpoint(endpointName, endpointURL)
+		configNoURL := privatelinkEndpoint(endpointName, "")
+
+		configNoName := privatelinkEndpoint("", endpointURL)
+
+		check := resource.ComposeAggregateTestCheckFunc(
+			resource.TestCheckResourceAttr("data.dbt_cloud_privatelink_endpoint.test", "name", endpointName),
+			resource.TestCheckResourceAttr("data.dbt_cloud_privatelink_endpoint.test", "private_link_endpoint_url", endpointURL),
+			resource.TestCheckResourceAttrSet("data.dbt_cloud_privatelink_endpoint.test", "id"),
+			resource.TestCheckResourceAttrSet("data.dbt_cloud_privatelink_endpoint.test", "cidr_range"),
+			resource.TestCheckResourceAttrSet("data.dbt_cloud_privatelink_endpoint.test", "type"),
+			resource.TestCheckResourceAttrSet("data.dbt_cloud_privatelink_endpoint.test", "state"),
+		)
+
+		resource.ParallelTest(t, resource.TestCase{
+			Providers: providers(),
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check:  check,
+				},
+				{
+					Config: configNoURL,
+					Check:  check,
+				},
+				{
+					Config: configNoName,
+					Check:  check,
+				},
+			},
+		})
+	} else {
+		log.Println("WARNING: The test is skipped as DBT_TEST_PRIVATE_LINK is not set")
+	}
+}
+
+func privatelinkEndpoint(endpointName, endpointURL string) string {
+	return fmt.Sprintf(`
+	data "dbt_cloud_privatelink_endpoint" "test" {
+		name = "%s"
+		private_link_endpoint_url = "%s"
+	  }
+    `, endpointName, endpointURL)
+}
---
+++ pkg/dbt_cloud/privatelink_endpoint.go
@@ -0,0 +1,59 @@
+package dbt_cloud
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type PrivatelinkEndpoint struct {
+	Account_Id             int    `json:"account_id"`
+	Name                   string `json:"name"`
+	Type                   string `json:"type"`
+	PrivatelinkEndpointURL string `json:"private_link_endpoint"`
+	CIDRRange              string `json:"cidr_range"`
+	State                  int    `json:"state"`
+	ID                     string `json:"id"`
+}
+
+type PrivatelinkEndpointListResponse struct {
+	Data   []PrivatelinkEndpoint `json:"data"`
+	Status ResponseStatus        `json:"status"`
+}
+
+type PrivatelinkEndpointResponse struct {
+	Data   PrivatelinkEndpoint `json:"data"`
+	Status ResponseStatus      `json:"status"`
+}
+
+func (c *Client) GetPrivatelinkEndpoint(endpointName string, privatelinkEndpointURL string) (*PrivatelinkEndpoint, error) {
+
+	if endpointName == "" && privatelinkEndpointURL == "" {
+		return nil, fmt.Errorf("The endpoint name or url needs to be provided")
+	}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/v3/accounts/%d/private-link-endpoints/", c.HostURL, c.AccountID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	PrivatelinkEndpointListResponse := PrivatelinkEndpointListResponse{}
+	err = json.Unmarshal(body, &PrivatelinkEndpointListResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	for i, endpoint := range PrivatelinkEndpointListResponse.Data {
+		if (endpointName == "" || endpoint.Name == endpointName) &&
+			(privatelinkEndpointURL == "" || endpoint.PrivatelinkEndpointURL == privatelinkEndpointURL) {
+			return &PrivatelinkEndpointListResponse.Data[i], nil
+		}
+	}
+
+	return nil, fmt.Errorf("Did not find PrivateLink endpoint with name = '%s' and/or endpoint = '%s'", endpointName, privatelinkEndpointURL)
+}
