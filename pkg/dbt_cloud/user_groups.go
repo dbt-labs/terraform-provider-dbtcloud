ca1bb8158cabece9b7decb33474dcee44b3b2bc3
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-08-03 14:21:32 +0200

    Update logic to use the /users/ endpoint The /users/id/ returns a 500 for service tokens

--- pkg/dbt_cloud/user_groups.go
+++ pkg/dbt_cloud/user_groups.go
@@ -38,8 +38,41 @@ type AssignUserGroupsResponse struct {
 	Status ResponseStatus `json:"status"`
 }
 
+// the 2 following types are used to parse groups from /users/
+// they could be removed once we can call /users/{user_id}/
+type UserWithGroups struct {
+	ID     int          `json:"id"`
+	Email  string       `json:"email"`
+	Groups []Permission `json:"permissions"`
+}
+
+type UserListResponseWithGroups struct {
+	Data   []UserWithGroups `json:"data"`
+	Status ResponseStatus   `json:"status"`
+}
+
 func (c *Client) GetUserGroups(userId int) (*UserGroupsCurrentAccount, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/v2/accounts/%s/users/%s/", c.HostURL, strconv.Itoa(c.AccountID), strconv.Itoa(userId)), nil)
+
+	// TODO: the current endpoint is not working for service tokens, we could use it when it is updated
+	// in the meantime we use the /users/ endpoint and parse the groups from there
+
+	// req, err := http.NewRequest("GET", fmt.Sprintf("%s/v2/accounts/%s/users/%s/", c.HostURL, strconv.Itoa(c.AccountID), strconv.Itoa(userId)), nil)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// body, err := c.doRequest(req)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// userGroupsResponse := UserGroupsResponse{}
+	// err = json.Unmarshal(body, &userGroupsResponse)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/v3/accounts/%s/users/?limit=1000", c.HostURL, strconv.Itoa(c.AccountID)), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -49,16 +82,23 @@ func (c *Client) GetUserGroups(userId int) (*UserGroupsCurrentAccount, error) {
 		return nil, err
 	}
 
-	userGroupsResponse := UserGroupsResponse{}
-	err = json.Unmarshal(body, &userGroupsResponse)
+	userListResponse := UserListResponseWithGroups{}
+	err = json.Unmarshal(body, &userListResponse)
 	if err != nil {
 		return nil, err
+	}
+
+	userGroupsPermissions := []Permission{}
+	for i, user := range userListResponse.Data {
+		if user.ID == userId {
+			userGroupsPermissions = userListResponse.Data[i].Groups
+		}
 	}
 
 	// the API returns the permissions for all accounts
 	// we just want to get the ones of the current account
 	userGroupsCurrentAccount := UserGroupsCurrentAccount{}
-	for _, permission := range userGroupsResponse.Data.Permissions {
+	for _, permission := range userGroupsPermissions {
 		if permission.AccountID == c.AccountID {
 			userGroupsCurrentAccount.Groups = append(userGroupsCurrentAccount.Groups, permission.Groups...)
 		}
