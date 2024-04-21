e4fdcbfb0f2f7423079213b10f29dd95e6669c7e
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-04-21 21:43:24 +0200

    Create first framework resource and datasource

---
+++ pkg/framework/objects/notification/data_source_acceptance_test.go
@@ -0,0 +1,84 @@
+package notification_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/framework/acctest_helper"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccDbtCloudNotificationDataSource(t *testing.T) {
+
+	randomProjectName := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+
+	config := notification(randomProjectName)
+
+	check := resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttr(
+			"data.dbtcloud_notification.test_notification_external",
+			"notification_type",
+			"4",
+		),
+		resource.TestCheckResourceAttrSet(
+			"data.dbtcloud_notification.test_notification_external",
+			"on_failure.0",
+		),
+		resource.TestCheckResourceAttr(
+			"data.dbtcloud_notification.test_notification_external",
+			"external_email",
+			"nomail@mail.com",
+		),
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest_helper.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check:  check,
+			},
+		},
+	})
+}
+
+func notification(projectName string) string {
+	return fmt.Sprintf(`
+	resource "dbtcloud_project" "test_notification_project" {
+		name = "%s"
+	}
+		
+	resource "dbtcloud_environment" "test_notification_environment" {
+		project_id  = dbtcloud_project.test_notification_project.id
+		name        = "Test Env Notification"
+		dbt_version = "%s"
+		type        = "development"
+	}
+		
+	resource "dbtcloud_job" "test_notification_job_1" {
+		name           = "Job 1 TF"
+		project_id     = dbtcloud_project.test_notification_project.id
+		environment_id = dbtcloud_environment.test_notification_environment.environment_id
+		execute_steps = [
+			"dbt compile"
+		]
+		triggers = {
+			"github_webhook" : false,
+			"git_provider_webhook" : false,
+			"schedule" : false,
+		}
+	}
+
+	resource "dbtcloud_notification" "test_notification_external" {
+		user_id           = 100
+		on_failure        = [dbtcloud_job.test_notification_job_1.id]
+		notification_type = 4
+		external_email    = "nomail@mail.com"
+	}
+
+	data "dbtcloud_notification" "test_notification_external" {
+		notification_id = dbtcloud_notification.test_notification_external.id
+	}
+    `, projectName, acctest_helper.DBT_CLOUD_VERSION)
+}
---
+++ pkg/framework/objects/notification/schema.go
@@ -0,0 +1,101 @@
+package notification
+
+import (
+	"context"
+
+	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/helper"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func (r *notificationResource) Schema(
+	_ context.Context,
+	_ resource.SchemaRequest,
+	resp *resource.SchemaResponse,
+) {
+	resp.Schema = schema.Schema{
+		Description: "Setup notifications on jobs success/failure to internal users, external email addresses or Slack channels",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:    true,
+				Description: "The ID of the notification",
+				// this is used so that we don't show that ID is going to change
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"user_id": schema.Int64Attribute{
+				Required:    true,
+				Description: "Internal dbt Cloud User ID. Must be the user_id for an existing user even if the notification is an external one",
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.RequiresReplace(),
+				},
+			},
+			"on_cancel": schema.SetAttribute{
+				ElementType: types.Int64Type,
+				Optional:    true,
+				Computed:    true,
+				Default:     helper.EmptySetDefault(types.Int64Type),
+				Description: "List of job IDs to trigger the webhook on cancel",
+			},
+			"on_failure": schema.SetAttribute{
+				ElementType: types.Int64Type,
+				Optional:    true,
+				Computed:    true,
+				Default:     helper.EmptySetDefault(types.Int64Type),
+				Description: "List of job IDs to trigger the webhook on failure",
+			},
+			"on_success": schema.SetAttribute{
+				ElementType: types.Int64Type,
+				Optional:    true,
+				Computed:    true,
+				Default:     helper.EmptySetDefault(types.Int64Type),
+				Description: "List of job IDs to trigger the webhook on success",
+			},
+			"state": schema.Int64Attribute{
+				Optional:    true,
+				Computed:    true,
+				Default:     int64default.StaticInt64(1),
+				Description: "State of the notification (1 = active (default), 2 = inactive)",
+			},
+			"notification_type": schema.Int64Attribute{
+				Optional:    true,
+				Computed:    true,
+				Default:     int64default.StaticInt64(1),
+				Description: "Type of notification (1 = dbt Cloud user email (default): does not require an external_email ; 2 = Slack channel: requires `slack_channel_id` and `slack_channel_name` ; 4 = external email: requires setting an `external_email`)",
+			},
+			"external_email": schema.StringAttribute{
+				Optional:    true,
+				Description: "The external email to receive the notification",
+				Validators: []validator.String{
+					stringvalidator.ConflictsWith(
+						path.MatchRoot("slack_channel_id"),
+						path.MatchRoot("slack_channel_name"),
+					),
+				},
+			},
+			"slack_channel_id": schema.StringAttribute{
+				Optional:    true,
+				Description: "The ID of the Slack channel to receive the notification. It can be found at the bottom of the Slack channel settings",
+				Validators: []validator.String{
+					stringvalidator.ConflictsWith(path.MatchRoot("external_email")),
+				},
+			},
+			"slack_channel_name": schema.StringAttribute{
+				Optional:    true,
+				Description: "The name of the slack channel",
+				Validators: []validator.String{
+					stringvalidator.ConflictsWith(path.MatchRoot("external_email")),
+				},
+			},
+		},
+	}
+}
