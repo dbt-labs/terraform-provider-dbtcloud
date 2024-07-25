5a917da0e19b4a5d714e6e3eae52aa52be911661
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-07-25 10:56:19 +0200

    Skeleton for resource acceptance test

---
+++ pkg/framework/objects/global_connection/resource_acceptance_test.go
@@ -0,0 +1,15 @@
+package global_connection_test
+
+import (
+	"testing"
+)
+
+func TestAccDbtCloudGlobalConnectionResource(t *testing.T) {
+
+	// TODO:
+	// - test that create/update works for all different connection types
+	// - have tests where we add/remove optional fields
+	// - have tests on the computed values
+	// - test the import as well
+
+}
