30ac6a07b0b084c9726e39a1ad864f7eb67f100b
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-10-23 11:54:22 +0200

    Set versionless as default for envs

--- examples/resources/dbtcloud_environment/resource.tf
+++ examples/resources/dbtcloud_environment/resource.tf
@@ -1,5 +1,5 @@
 resource "dbtcloud_environment" "ci_environment" {
-  // the dbt_version is major.minor.0-latest , major.minor.0-pre or versionless (Beta on 15 Feb 2024, to always be on the latest dbt version)
+  // the dbt_version is major.minor.0-latest , major.minor.0-pre or versionless (by default, it is set to versionless if not configured)
   dbt_version   = "versionless"
   name          = "CI"
   project_id    = dbtcloud_project.dbt_project.id
@@ -25,5 +25,5 @@ resource "dbtcloud_environment" "dev_environment" {
   name        = "Dev"
   project_id  = dbtcloud_project.dbt_project.id
   type        = "development"
-  connection_id = dbtcloud_global_connection.my_other_global_connection
+  connection_id = dbtcloud_global_connection.my_other_global_connection.id
 }
