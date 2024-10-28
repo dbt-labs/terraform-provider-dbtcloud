4024c63e6a8fa4e2494c30e812c8b67729ceea0f
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-10-28 15:56:50 +0100

    Update example for dbtcloud_snowflake_credential

--- examples/resources/dbtcloud_snowflake_credential/resource.tf
+++ examples/resources/dbtcloud_snowflake_credential/resource.tf
@@ -1,5 +1,5 @@
 resource "dbtcloud_snowflake_credential" "prod_credential" {
-  project_id  = data.dbtcloud_project.dbt_project.id
+  project_id  = dbtcloud_project.dbt_project.id
   auth_type   = "password"
   num_threads = 16
   schema      = "SCHEMA"
