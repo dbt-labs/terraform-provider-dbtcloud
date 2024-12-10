5fe466963bd0d5a7044fbbf1f65fbe1ee63b690e
Chase Walden <chase.walden@dbtlabs.com>
2024-12-10 14:40:11 -0700

    added token override

--- pkg/framework/objects/azure_dev_ops_project/data_source_acceptance_test.go
+++ pkg/framework/objects/azure_dev_ops_project/data_source_acceptance_test.go
@@ -11,7 +11,11 @@ import (
 
 func TestAccDbtCloudAzureDevOpsProject(t *testing.T) {
 	// TODO(cwalden): This cannot be run without ADO integration. We also cannot use a ServiceToken, which is the only token available in dbt Cloud CI.
-	t.Skip("Skipping AzureDevOps Project in dbt Cloud CI. This test cannot be run without ADO integration. We also cannot use a ServiceToken, which is the only token available in dbt Cloud CI.")
+	t.Skip(
+		"Skipping AzureDevOps Project in dbt Cloud CI. ",
+		"This test cannot be run without ADO integration. ",
+		"We also cannot use a ServiceToken, which is the only token available in dbt Cloud CI.",
+	)
 
 	if acctest_helper.IsDbtCloudPR() {
 		t.Skip("Skipping AzureDevOps Project in dbt Cloud CI for now")
@@ -26,9 +30,19 @@ func TestAccDbtCloudAzureDevOpsProject(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				ConfigVariables: config.Variables{
+					"dbt_token":        config.StringVariable("this-will-come-from-somewhere-else-eventually"), // Cannot be a Service Token
 					"ado_project_name": config.StringVariable(adoProjectName),
 				},
 				Config: `
+					variable "dbt_token" {
+						type = string
+						sensitive = true
+					}
+
+					provider "dbtcloud" {
+						token = var.dbt_token
+					}
+
 					variable "ado_project_name" {
 						type = string
 					}
