7045038e409867e541644b8861ce3faad3f7f65d
Adrian Burus <adrian.burus@dbtlabs.com>
2025-03-27 10:38:56 +0200

    Added defaults and fixed mandatory fields

--- pkg/framework/objects/environment/schema.go
+++ pkg/framework/objects/environment/schema.go
@@ -3,10 +3,17 @@ package environment
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	resource_schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
 func (r *environmentDataSource) Schema(
@@ -150,11 +157,13 @@ func (r *environmentResource) Schema(
 		Description: "Retrieve data for a single environment",
 		Attributes: map[string]resource_schema.Attribute{
 			"environment_id": resource_schema.Int64Attribute{
-				Required:    true,
+				Computed:    true,
 				Description: "The ID of the environment",
 			},
 			"is_active": resource_schema.BoolAttribute{
-				Required:    true,
+				Optional:    true,
+				Computed:    true,
+				Default:     booldefault.StaticBool(true),
 				Description: "Whether the environment is active",
 			},
 			"project_id": resource_schema.Int64Attribute{
@@ -162,35 +171,55 @@ func (r *environmentResource) Schema(
 				Description: "(Number) Project ID to create the environment in",
 			},
 			"credentials_id": resource_schema.Int64Attribute{
+				Optional:    true,
 				Computed:    true,
 				Description: "The project ID to which the environment belong",
 			},
 			"name": resource_schema.StringAttribute{
-				Computed:    true,
+				Required:    true,
 				Description: "The name of the environment",
 			},
 			"dbt_version": resource_schema.StringAttribute{
 				Computed:    true,
+				Optional:    true,
+				Default:     stringdefault.StaticString("latest"),
 				Description: "(String) Version number of dbt to use in this environment. It needs to be in the format `major.minor.0-latest` (e.g. `1.5.0-latest`), `major.minor.0-pre`, `versionless`, or `latest`. While `versionless` is still supported, using `latest` is recommended. Defaults to `latest` if no version is provided",
+				Validators: []validator.String{
+					stringvalidator.OneOf("latest", "versionless"),
+				},
 			},
 			"type": resource_schema.StringAttribute{
-				Computed:    true,
+				Required:    true,
 				Description: "(String) The type of environment (must be either development or deployment)",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+				Validators: []validator.String{
+					stringvalidator.OneOf("development", "deployment"),
+				},
 			},
 			"use_custom_branch": resource_schema.BoolAttribute{
 				Computed:    true,
+				Optional:    true,
+				Default:     booldefault.StaticBool(false),
 				Description: "Whether to use a custom git branch in this environment",
 			},
 			"custom_branch": resource_schema.StringAttribute{
 				Computed:    true,
+				Optional:    true,
+				Default:     stringdefault.StaticString(""),
 				Description: "The custom branch name to use",
 			},
 			"deployment_type": resource_schema.StringAttribute{
 				Computed:    true,
+				Optional:    true,
+				Default:     stringdefault.StaticString(""),
 				Description: "(String) The type of environment. Only valid for environments of type 'deployment' and for now can only be 'production', 'staging' or left empty for generic environments",
 			},
 			"extended_attributes_id": resource_schema.Int64Attribute{
 				Computed:    true,
+				Optional:    true,
+				Default:     int64default.StaticInt64(0),
 				Description: "The ID of the extended attributes applied",
 			},
 			"connection_id": resource_schema.Int64Attribute{
@@ -199,6 +228,8 @@ func (r *environmentResource) Schema(
 			},
 			"enable_model_query_history": resource_schema.BoolAttribute{
 				Computed:    true,
+				Optional:    true,
+				Default:     booldefault.StaticBool(false),
 				Description: "(Boolean) Whether to enable model query history in this environment. As of Oct 2024, works only for Snowflake and BigQuery.",
 			},
 		},
