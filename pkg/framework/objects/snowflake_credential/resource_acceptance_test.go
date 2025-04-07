de22f31aed36f77f63dece0778bd193e9c654626
Adrian <adrian.burus@dbtlabs.com>
2025-04-07 10:19:02 +0300

    Migrate snowflake credential data-source & resource (#376)
    * Migrate snowflake credential data-source
    
    * Remove sdkv2 data-source
    
    * Migrate snowflake credential resource
    
    * Add conformance tests and fix issues
    
    * Fixed conformance tests.
    
    * Update docs

--- pkg/framework/objects/snowflake_credential/resource_acceptance_test.go
+++ pkg/framework/objects/snowflake_credential/resource_acceptance_test.go
@@ -1,4 +1,4 @@
-package resources_test
+package snowflake_credential_test
 
 import (
 	"fmt"
@@ -6,12 +6,144 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/framework/acctest_config"
 	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/framework/acctest_helper"
 	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/helper"
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
+
+func getBasicConfigTestStep(projectName, database, role, warehouse, schema, user, password string) resource.TestStep {
+	return resource.TestStep{
+		Config: testAccDbtCloudSnowflakeCredentialResourceBasicConfig(
+			projectName,
+			database,
+			role,
+			warehouse,
+			schema,
+			user,
+			password,
+		),
+		Check: resource.ComposeTestCheckFunc(
+			testAccCheckDbtCloudSnowflakeCredentialExists(
+				"dbtcloud_snowflake_credential.test_credential",
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"database",
+				database,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"role",
+				role,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"warehouse",
+				warehouse,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"schema",
+				schema,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"user",
+				user,
+			),
+		),
+	}
+}
+
+func getModifyConfigTestStep(projectName, database, role, warehouse, schema, user, password string) resource.TestStep {
+	return resource.TestStep{
+		Config: testAccDbtCloudSnowflakeCredentialResourceBasicConfig(
+			projectName,
+			database,
+			role,
+			warehouse,
+			schema,
+			user,
+			password,
+		),
+		Check: resource.ComposeTestCheckFunc(
+			testAccCheckDbtCloudSnowflakeCredentialExists(
+				"dbtcloud_snowflake_credential.test_credential",
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"database",
+				database,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"role",
+				role,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"warehouse",
+				warehouse,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"schema",
+				schema,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"user",
+				user,
+			),
+			resource.TestCheckResourceAttr(
+				"dbtcloud_snowflake_credential.test_credential",
+				"password",
+				password,
+			),
+		),
+	}
+}
+
+func TestBasicConfigConformance(t *testing.T) {
+	projectName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	database := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	role := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	warehouse := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	schema := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	user := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	password := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { acctest_helper.TestAccPreCheck(t) },
+		CheckDestroy: testAccCheckDbtCloudSnowflakeCredentialDestroy,
+		Steps: []resource.TestStep{
+			acctest_helper.MakeExternalProviderTestStep(getBasicConfigTestStep(projectName, database, role, warehouse, schema, user, password), acctest_config.LAST_VERSION_BEFORE_FRAMEWORK_MIGRATION),
+			acctest_helper.MakeCurrentProviderNoOpTestStep(getBasicConfigTestStep(projectName, database, role, warehouse, schema, user, password)),
+		},
+	})
+}
+
+func TestModifyConfigConformance(t *testing.T) {
+	projectName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	database2 := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	role2 := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	warehouse2 := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	schema2 := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	user2 := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	password2 := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { acctest_helper.TestAccPreCheck(t) },
+		CheckDestroy: testAccCheckDbtCloudSnowflakeCredentialDestroy,
+		Steps: []resource.TestStep{
+			acctest_helper.MakeExternalProviderTestStep(getModifyConfigTestStep(projectName, database2, role2, warehouse2, schema2, user2, password2), acctest_config.LAST_VERSION_BEFORE_FRAMEWORK_MIGRATION),
+			acctest_helper.MakeCurrentProviderNoOpTestStep(getModifyConfigTestStep(projectName, database2, role2, warehouse2, schema2, user2, password2)),
+		},
+	})
+}
 
 func TestAccDbtCloudSnowflakeCredentialResource(t *testing.T) {
 
@@ -34,111 +166,26 @@ func TestAccDbtCloudSnowflakeCredentialResource(t *testing.T) {
 	privateKeyPassphrase := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
+		PreCheck:                 func() { acctest_helper.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: acctest_helper.TestAccProtoV6ProviderFactories,
 		CheckDestroy:             testAccCheckDbtCloudSnowflakeCredentialDestroy,
 		Steps: []resource.TestStep{
-			{
-				Config: testAccDbtCloudSnowflakeCredentialResourceBasicConfig(
-					projectName,
-					database,
-					role,
-					warehouse,
-					schema,
-					user,
-					password,
-				),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDbtCloudSnowflakeCredentialExists(
-						"dbtcloud_snowflake_credential.test_credential",
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"database",
-						database,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"role",
-						role,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"warehouse",
-						warehouse,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"schema",
-						schema,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"user",
-						user,
-					),
-				),
-			},
+			getBasicConfigTestStep(projectName, database, role, warehouse, schema, user, password),
 			// RENAME
 			// MODIFY
-			{
-				Config: testAccDbtCloudSnowflakeCredentialResourceBasicConfig(
-					projectName,
-					database2,
-					role2,
-					warehouse2,
-					schema2,
-					user2,
-					password2,
-				),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDbtCloudSnowflakeCredentialExists(
-						"dbtcloud_snowflake_credential.test_credential",
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"database",
-						database2,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"role",
-						role2,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"warehouse",
-						warehouse2,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"schema",
-						schema2,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"user",
-						user2,
-					),
-					resource.TestCheckResourceAttr(
-						"dbtcloud_snowflake_credential.test_credential",
-						"password",
-						password2,
-					),
-				),
-			},
+			getModifyConfigTestStep(projectName, database2, role2, warehouse2, schema2, user2, password2),
 			// IMPORT
 			{
 				ResourceName:            "dbtcloud_snowflake_credential.test_credential",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"password"},
+				ImportStateVerifyIgnore: []string{"password", "private_key", "private_key_passphrase"},
 			},
 		},
 	})
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
+		PreCheck:                 func() { acctest_helper.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: acctest_helper.TestAccProtoV6ProviderFactories,
 		CheckDestroy:             testAccCheckDbtCloudSnowflakeCredentialDestroy,
 		Steps: []resource.TestStep{
@@ -205,6 +252,7 @@ func TestAccDbtCloudSnowflakeCredentialResource(t *testing.T) {
 			},
 		},
 	})
+
 }
 
 func testAccDbtCloudSnowflakeCredentialResourceBasicConfig(
