73709e02fc9ceee5216caac9803c6e8e4ab90722
Vlad Moldoveanu <vlad.moldoveanu@dbtlabs.com>
2025-04-10 15:52:21 +0300

    Add create method

--- pkg/framework/objects/job/resource.go
+++ pkg/framework/objects/job/resource.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/dbt_cloud"
 	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/helper"
+	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/utils"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
@@ -37,8 +38,153 @@ func (j *jobResource) Configure(_ context.Context, req resource.ConfigureRequest
 	j.client = req.ProviderData.(*dbt_cloud.Client)
 }
 
-func (j *jobResource) Create(context.Context, resource.CreateRequest, *resource.CreateResponse) {
-	panic("unimplemented")
+func (j *jobResource) Create(ctx context.Context,req resource.CreateRequest,resp *resource.CreateResponse) {
+	var plan JobResourceModel
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	projectId := plan.ProjectID
+	environmentId := plan.EnvironmentID
+	name := plan.Name.ValueString()
+	description := plan.Description.ValueString()
+
+	executeSteps := make([]string, len(plan.ExecuteSteps))
+	for i, step := range plan.ExecuteSteps {
+		executeSteps[i] = step.ValueString()
+	}
+
+	var dbtVersion *string
+	if !plan.DbtVersion.IsNull() {
+		dbtVersionValue := plan.DbtVersion.ValueString()
+		dbtVersion = &dbtVersionValue
+	}
+
+	isActive := plan.IsActive.ValueBool()
+	triggers := map[string]any{
+		"github_webhook":       plan.Triggers.GithubWebhook.ValueBool(),
+		"git_provider_webhook": plan.Triggers.GitProviderWebhook.ValueBool(),
+		"schedule":           plan.Triggers.Schedule.ValueBool(),
+		"on_merge":           plan.Triggers.OnMerge.ValueBool(),
+	}
+	numThreads := int(plan.NumThreads.ValueInt64())
+	targetName := plan.TargetName.ValueString()
+	generateDocs := plan.GenerateDocs.ValueBool()
+	runGenerateSources := plan.RunGenerateSources.ValueBool()
+	scheduleType := plan.ScheduleType.ValueString()
+	scheduleInterval := int(plan.ScheduleInterval.ValueInt64())
+
+	scheduleHours := make([]int, len(plan.ScheduleHours))
+	for i, hour := range plan.ScheduleHours {
+		scheduleHours[i] = int(hour.ValueInt64())
+	}
+
+	scheduleDays := make([]int, len(plan.ScheduleDays))
+	for i, day := range plan.ScheduleDays {
+		scheduleDays[i] = int(day.ValueInt64())
+	}
+
+	scheduleCron := plan.ScheduleCron.ValueString()
+
+	var deferringJobId *int
+	if !plan.DeferringJobId.IsNull() {
+		deferringJobId = helper.Int64ToIntPointer(plan.DeferringJobId.ValueInt64())
+	}
+
+	var deferringEnvironmentID *int
+	if !plan.DeferringEnvironmentID.IsNull() {
+		deferringEnvironmentID = helper.Int64ToIntPointer(plan.DeferringEnvironmentID.ValueInt64())
+	}
+
+	selfDeferring := plan.SelfDeferring.ValueBool()
+	timeoutSeconds := int(plan.TimeoutSeconds.ValueInt64())
+	triggersOnDraftPR := plan.TriggersOnDraftPr.ValueBool()
+
+	var jobCompletionTriggerCondition map[string]any
+	if plan.JobCompletionTriggerCondition != nil {
+		condition := plan.JobCompletionTriggerCondition.Condition
+		statuses := make([]int, len(condition.Statuses))
+		for i, status := range condition.Statuses {
+			statuses[i] = utils.JobCompletionTriggerConditionsMappingHumanCode[status.ValueString()]
+		}
+		jobCompletionTriggerCondition = map[string]any{
+			"job_id":     int(condition.JobID.ValueInt64()),
+			"project_id": int(condition.ProjectID.ValueInt64()),
+			"statuses":   statuses,
+		}
+	}
+
+	runCompareChanges := plan.RunCompareChanges.ValueBool()
+	runLint := plan.RunLint.ValueBool()
+	errorsOnLintFailure := plan.ErrorsOnLintFailure.ValueBool()
+	jobType := plan.JobType.ValueString()
+	compareChangesFlags := plan.CompareChangesFlags.ValueString()
+
+	createDbtVersion := ""
+	if dbtVersion != nil {
+		createDbtVersion = *dbtVersion
+	}
+	createDeferringJobID := 0
+	if deferringJobId != nil {
+		createDeferringJobID = *deferringJobId
+	}
+	createDeferringEnvironmentID := 0
+	if deferringEnvironmentID != nil {
+		createDeferringEnvironmentID = *deferringEnvironmentID
+	}
+
+	createdJob, err := j.client.CreateJob(int(projectId.ValueInt64()),
+		int(environmentId.ValueInt64()),
+		name,
+		description,
+		executeSteps,
+		createDbtVersion, // Use safe value
+		isActive,
+		triggers,
+		numThreads,
+		targetName,
+		generateDocs,
+		runGenerateSources,
+		scheduleType,
+		scheduleInterval,
+		scheduleHours,
+		scheduleDays,
+		scheduleCron,
+		createDeferringJobID, // Use safe value
+		createDeferringEnvironmentID, // Use safe value
+		selfDeferring,
+		timeoutSeconds,
+		triggersOnDraftPR,
+		jobCompletionTriggerCondition,
+		runCompareChanges,
+		runLint,
+		errorsOnLintFailure,
+		jobType,
+		compareChangesFlags,
+	)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error creating job",
+			"Could not create job, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	if createdJob != nil && createdJob.ID != nil {
+		plan.ID = types.Int64Value(int64(*createdJob.ID))
+		plan.JobId= types.Int64Value(int64(*createdJob.ID))
+	} else {
+		resp.Diagnostics.AddError(
+			"Error creating job",
+			"Created job or its ID is unexpectedly nil",
+		)
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
 }
 
 func (j *jobResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
