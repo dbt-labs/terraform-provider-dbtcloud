e79ae4ba56724f76429c7da810694f8fb2d30374
Adrian <adrian.burus@dbtlabs.com>
2025-05-06 13:10:27 +0300

    Enable AI features and Warehouse Cost Visibility account features (#407)
    * Enable AI features and Warehouse Cost Visibility account features
    
    * Removing warehouse cost visibility from acceptance test as it fails due to account entitlements

--- pkg/framework/objects/account_features/model.go
+++ pkg/framework/objects/account_features/model.go
@@ -5,8 +5,10 @@ import (
 )
 
 type AccountFeaturesResourceModel struct {
-	ID             types.String `tfsdk:"id"`
-	AdvancedCI     types.Bool   `tfsdk:"advanced_ci"`
-	PartialParsing types.Bool   `tfsdk:"partial_parsing"`
-	RepoCaching    types.Bool   `tfsdk:"repo_caching"`
+	ID                      types.String `tfsdk:"id"`
+	AdvancedCI              types.Bool   `tfsdk:"advanced_ci"`
+	PartialParsing          types.Bool   `tfsdk:"partial_parsing"`
+	RepoCaching             types.Bool   `tfsdk:"repo_caching"`
+	AIFeatures              types.Bool   `tfsdk:"ai_features"`
+	WarehouseCostVisibility types.Bool   `tfsdk:"warehouse_cost_visibility"`
 }
