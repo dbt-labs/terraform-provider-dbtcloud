239c161adf56ce7ee7b3a291546a958cbf725d38
Victor Catana <46373894+victorasu@users.noreply.github.com>
2025-05-16 15:32:07 +0300

    Revert "Check if fields are unknown before asessing if they are null or empty (#423)"
    This reverts commit 2fe2006b3bf10e0ab0bd29ea3e7f3e15321a0f29.

--- pkg/framework/objects/snowflake_credential/validators/sl_cred_validator.go
+++ pkg/framework/objects/snowflake_credential/validators/sl_cred_validator.go
@@ -21,47 +21,34 @@ func (v SemanticLayerCredentialValidator) ValidateBool(ctx context.Context, req 
 
 	// Check if `semantic_layer_credential` is false
 	if !req.ConfigValue.ValueBool() || req.ConfigValue.IsUnknown() || req.ConfigValue.IsNull() {
-		var semantic_layer_enabled types.Bool
-		semanticLayerPath := req.Path.ParentPath().AtName("semantic_layer_credential")
-		diags := req.Config.GetAttribute(ctx, semanticLayerPath, &semantic_layer_enabled)
+		// Validate that `user` is provided
+		var userValue types.String
+		userPath := req.Path.ParentPath().AtName("user")
+		diags := req.Config.GetAttribute(ctx, userPath, &userValue)
 		if diags.HasError() {
 			resp.Diagnostics.Append(diags...)
 			return
 		}
-		if semantic_layer_enabled.IsNull() || !semantic_layer_enabled.ValueBool() {
-			// Validate that `user` is provided
-			var userValue types.String
-			userPath := req.Path.ParentPath().AtName("user")
-			diags = req.Config.GetAttribute(ctx, userPath, &userValue)
-			if diags.HasError() {
-				resp.Diagnostics.Append(diags...)
-				return
-			}
-			if !userValue.IsUnknown() {
-				if userValue.IsNull() || userValue.ValueString() == "" {
-					resp.Diagnostics.AddError(
-						"Missing Required Attribute",
-						"`user` must be provided when `semantic_layer_credential` is false.",
-					)
-				}
-			}
+		if userValue.IsNull() || userValue.IsUnknown() {
+			resp.Diagnostics.AddError(
+				"Missing Required Attribute",
+				"`user` must be provided when `semantic_layer_credential` is false.",
+			)
+		}
 
-			// Validate that `schema` is provided
-			var schemaValue types.String
-			schemaPath := req.Path.ParentPath().AtName("schema")
-			diags = req.Config.GetAttribute(ctx, schemaPath, &schemaValue)
-			if diags.HasError() {
-				resp.Diagnostics.Append(diags...)
-				return
-			}
-			if !schemaValue.IsUnknown() {
-				if schemaValue.IsNull() || schemaValue.ValueString() == "" {
-					resp.Diagnostics.AddError(
-						"Missing Required Attribute",
-						"`schema` must be provided when `semantic_layer_credential` is false.",
-					)
-				}
-			}
+		// Validate that `schema` is provided
+		var schemaValue types.String
+		schemaPath := req.Path.ParentPath().AtName("schema")
+		diags = req.Config.GetAttribute(ctx, schemaPath, &schemaValue)
+		if diags.HasError() {
+			resp.Diagnostics.Append(diags...)
+			return
+		}
+		if schemaValue.IsNull() || schemaValue.IsUnknown() {
+			resp.Diagnostics.AddError(
+				"Missing Required Attribute",
+				"`schema` must be provided when `semantic_layer_credential` is false.",
+			)
 		}
 	}
 }
