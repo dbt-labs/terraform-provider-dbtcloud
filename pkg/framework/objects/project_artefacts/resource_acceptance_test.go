17579423571eb6b668912d26f766c142eb254d2b
Victor Catana <46373894+victorasu@users.noreply.github.com>
2025-05-16 15:39:18 +0300

    Revert "Merge pull request #366 from dbt-labs/manual-revert-to-working"
    This reverts commit 2dc6b74f2026742450cec9a600a97a64818144b8, reversing
    changes made to 86aab285690600287b1c6c37614cbbb713da2e22.

--- pkg/framework/objects/project_artefacts/resource_acceptance_test.go
+++ pkg/framework/objects/project_artefacts/resource_acceptance_test.go
@@ -2,10 +2,11 @@ package project_artefacts_test
 
 import (
 	"fmt"
-	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/framework/acctest_config"
 	"regexp"
 	"strings"
 	"testing"
+
+	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/framework/acctest_config"
 
 	"github.com/dbt-labs/terraform-provider-dbtcloud/pkg/framework/acctest_helper"
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
