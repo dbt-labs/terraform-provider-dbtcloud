99a4a2e196c7a093ae8905f4543fea1fa9695738
Adrian Burus <adrian.burus@dbtlabs.com>
2025-08-28 14:04:58 +0300

    Fixed remote url and env id

--- testing/cicd/main.tf
+++ testing/cicd/main.tf
@@ -44,13 +44,13 @@ resource "dbtcloud_environment" "test_environment" {
 
 resource "dbtcloud_repository" "test_repository" {
   project_id = dbtcloud_project.test_project.id
-  remote_url = "https://github.com/dbt-labs/dbt-starter-project"
+  remote_url = "git@github.com:dbt-labs/dbt-starter-project.git"
   git_clone_strategy = "github_app"
 }
 
 resource "dbtcloud_job" "test_job" {
   project_id = dbtcloud_project.test_project.id
-  environment_id = dbtcloud_environment.test_environment.id
+  environment_id = dbtcloud_environment.test_environment.environment_id
   name = "Test Job"
   execute_steps = [
     "dbt run"
