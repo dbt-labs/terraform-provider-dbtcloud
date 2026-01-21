b07d53fc316e6b88879c8ca5ed7061fa72b1f30d
Adrian Burus <adrian.burus@dbtlabs.com>
2026-01-21 10:52:29 +0200

    Revert "Only check deployment_env_auth_type when latest adapter is used"
    This reverts commit 38cd996f6a95a1575eb538c9c3a6165bf4d69836.

--- pkg/framework/objects/global_connection/resource.go
+++ pkg/framework/objects/global_connection/resource.go
@@ -251,9 +251,7 @@ func (r *globalConnectionResource) Create(
 				plan.BigQueryConfig.DataprocClusterName.ValueString(),
 			)
 		}
-		// Only send deployment_env_auth_type for v1 adapter (use_latest_adapter = true)
-		// The v0 (legacy) adapter does not support this field
-		if plan.BigQueryConfig.UseLatestAdapter.ValueBool() && !plan.BigQueryConfig.DeploymentEnvAuthType.IsNull() {
+		if !plan.BigQueryConfig.DeploymentEnvAuthType.IsNull() {
 			bigqueryCfg.DeploymentEnvAuthType.Set(
 				plan.BigQueryConfig.DeploymentEnvAuthType.ValueString(),
 			)
@@ -978,17 +976,13 @@ func (r *globalConnectionResource) Update(
 				)
 			}
 		}
-		// Only send deployment_env_auth_type for v1 adapter (use_latest_adapter = true)
-		// The v0 (legacy) adapter does not support this field
-		if plan.BigQueryConfig.UseLatestAdapter.ValueBool() {
-			if plan.BigQueryConfig.DeploymentEnvAuthType != state.BigQueryConfig.DeploymentEnvAuthType {
-				if plan.BigQueryConfig.DeploymentEnvAuthType.IsNull() {
-					warehouseConfigChanges.DeploymentEnvAuthType.SetNull()
-				} else {
-					warehouseConfigChanges.DeploymentEnvAuthType.Set(
-						plan.BigQueryConfig.DeploymentEnvAuthType.ValueString(),
-					)
-				}
+		if plan.BigQueryConfig.DeploymentEnvAuthType != state.BigQueryConfig.DeploymentEnvAuthType {
+			if plan.BigQueryConfig.DeploymentEnvAuthType.IsNull() {
+				warehouseConfigChanges.DeploymentEnvAuthType.SetNull()
+			} else {
+				warehouseConfigChanges.DeploymentEnvAuthType.Set(
+					plan.BigQueryConfig.DeploymentEnvAuthType.ValueString(),
+				)
 			}
 		}
 
