6b68c6a6951dbedff5f7ef1d5e9ff08165fce45c
Sebastian Kapfer <sebastian.kapfer@dbtlabs.com>
2026-08-10 18:38:02 +0200

    chore: Delete dead code

--- pkg/dbt_cloud/adapter.go
+++ pkg/dbt_cloud/adapter.go
@@ -1,29 +1,5 @@
 package dbt_cloud
 
-type Adapter struct {
-	ID                      *int            `json:"id,omitempty"`
-	AccountID               int64           `json:"account_id"`
-	ProjectID               int             `json:"project_id"`
-	CreatedByID             *int            `json:"created_by_id,omitempty"`
-	CreatedByServiceTokenID *int            `json:"created_by_service_token_id,omitempty"`
-	Metadata                AdapterMetadata `json:"metadata_json"`
-	State                   int             `json:"state"`
-	AdapterVersion          string          `json:"adapter_version"`
-	CreatedAt               *string         `json:"created_at,omitempty"`
-	UpdatedAt               *string         `json:"updated_at,omitempty"`
-}
-
-type AdapterMetadata struct {
-	Title     string `json:"title"`
-	DocsLink  string `json:"docs_link"`
-	ImageLink string `json:"image_link"`
-}
-
-type AdapterResponse struct {
-	Data   Adapter        `json:"data"`
-	Status ResponseStatus `json:"status"`
-}
-
 type AdapterCredentialFieldMetadataValidation struct {
 	Required bool `json:"required"`
 }
